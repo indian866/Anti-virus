b187adf674c6a8722c8fae61f6abb7afb6122e3b
indian866 <indian866.06@gmail.com>
2020-06-01 07:37:21 +0530

    initial commit

---
+++ Antivirus.sh
@@ -0,0 +1,184 @@
+@echo off
+color a
+echo Welcome to indian866 's antivirus
+
+
+pause
+c:
+a:
+attrib -h -s -s -a /s /d a:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+b:
+attrib -h -s -s -a /s /d b:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+d:
+attrib -h -s -s -a /s /d d:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+e:
+attrib -h -s -s -a /s /d e:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+f:
+attrib -h -s -s -a /s /d f:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+g:
+attrib -h -s -s -a /s /d g:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+h:
+attrib -h -s -s -a /s /d h:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+i:
+attrib -h -s -s -a /s /d i:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+j:
+attrib -h -s -s -a /s /d j:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+k:
+attrib -h -s -s -a /s /d k:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+l:
+attrib -h -s -s -a /s /d l:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+m:
+attrib -h -s -s -a /s /d m:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+n:
+attrib -h -s -s -a /s /d n:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+o:
+attrib -h -s -s -a /s /d o:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+p:
+attrib -h -s -s -a /s /d p:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+q:
+attrib -h -s -s -a /s /d q:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+r:
+attrib -h -s -s -a /s /d r:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+s:
+attrib -h -s -s -a /s /d s:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+t:
+attrib -h -s -s -a /s /d t:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+u:
+attrib -h -s -s -a /s /d u:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+v:
+attrib -h -s -s -a /s /d v:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+w:
+attrib -h -s -s -a /s /d w:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+x:
+attrib -h -s -s -a /s /d x:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+y:
+attrib -h -s -s -a /s /d y:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+c:
+z:
+attrib -h -s -s -a /s /d z:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+
+
+echo ALL VIRUS ARE DELETED FROM YOUR LAPTOP OR COMPUTER
+pause
---
+++ README.md
@@ -0,0 +1,65 @@
+<h1 align="center">Anti-virus v1.1</h1>
+<p align="center">
+     A new way to End The virus
+</p>
+
+## ?? ***About Antivirus***:
+
+This Antivirus Help You To Kill The Virus From Computer or laptop . This Officially made for The termux . This clean The virus from Your pc and Computer.
+
+
+### ?? ***Antivirus is available for***:
+
+* Termux
+
+### ?? ***Installation and usage guide***:
+```
+$ apt-get update -y
+```
+```
+$ apt-get upgrade -y
+```
+```
+$ pkg install python -y 
+```
+```
+$ pkg install python2 -y
+```
+```
+$ pkg install git -y
+```
+```
+$ pip install lolcat
+```
+```
+$ git clone https://github.com/indian866/Antivirus
+```
+```
+$ ls
+```
+```
+$ cd infect
+```
+```
+$ ls
+```
+```
+$ bash Antivirus.sh
+```
+* Now you need internet connection to continue further process...
+
+* You can select any option by clicking on your keyboard=&
+
+### Subscribe our channel on youtube:
+https://www.youtube.com/indian866
+
+### Instagram: 
+https://www.instagram.com/indi_an866
+
+### facebook:
+https://www.facebook.com/indian866
+
+Discription: use This Anti-virus for  kill The virus in your pc or computer.
+
+
+*************************THANK YOU*************************
